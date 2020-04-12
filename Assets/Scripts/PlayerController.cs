1d274e2b854bea251a123edc73d8db80988aa25f
RamBanjo <rambanjo@gmail.com>
2020-04-12 16:19:36 +0700

    initial commit.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 5;
+    public Camera main_camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //movement, get axis
+        float vert = Input.GetAxis("Vertical") * speed;
+        float horz = Input.GetAxis("Horizontal") * speed;
+
+        vert *= Time.deltaTime;
+        horz *= Time.deltaTime;
+
+        //and then move the character
+        transform.Translate(horz, vert, 0);
+    }
+}
