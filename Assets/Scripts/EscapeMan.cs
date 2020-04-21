00ef4e6b22589048f5c04efbe3c628826b393a48
RamBanjo <rambanjo@gmail.com>
2020-04-22 03:03:20 +0700

    Implemented man who runs away

--- Assets/Scripts/EscapeMan.cs
+++ Assets/Scripts/EscapeMan.cs
@@ -3,15 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EscapeMan : InvisibleMan {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public Collider2D activateRadius;
+    public Collider2D playerRadius;
+
+    private void Update() {
+        Movement();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    new public void Movement() {
+
+        if (activateRadius.IsTouching(playerRadius)) {
+            RunAway();
+        } else {
+            MoveToDestination();
+        }
+
     }
+
+    new public void MoveToDestination() {
+        if (destination == null) {
+            destination = GetClosestWaypoint();
+        }
+
+        base.MoveToDestination();
+
+    }
+
+    private void RunAway() {
+        if (destination != null) destination = null;
+
+        Vector2 myPosition = transform.position;
+        Vector2 playerLocation = playerRadius.transform.position;
+        Vector2 moveDir = myPosition - playerLocation;
+
+        int stopWhenCaught = 1;
+
+        if (caught) {
+            stopWhenCaught = 0;
+        }
+
+        transform.Translate(moveDir.normalized * speed * Time.deltaTime * stopWhenCaught);
+
+    }
+
 }
