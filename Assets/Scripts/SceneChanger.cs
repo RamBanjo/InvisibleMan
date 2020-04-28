dbc2cb24f0ebbf2bf09193e48688aa06caac6d8c
RamBanjo <rambanjo@gmail.com>
2020-04-29 01:52:05 +0700

    updated credits and instructions to make it appear more properly

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -5,22 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public static SceneChanger instance;
-
-    private void Awake() {
-        if (instance == null) {
-            instance = this;
-        } else {
-            Destroy(this);
-        }
-    }
-
     public void ChangeScene(string scene) {
         SceneManager.LoadScene(scene);
-    }
-
-    public static void ChangeSceneStatic(string scene) {
-        instance.ChangeScene(scene);
     }
 
     public void QuitGame() {
